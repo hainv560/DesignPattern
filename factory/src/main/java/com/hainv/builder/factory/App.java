0b7b5e03740563b9c2a115a7283fc2ba9f64d81f
DuyNx <duynx@oneempower.com>
2018-03-03 00:41:24 +0700

    Initial Version

---
+++ factory/src/main/java/com/hainv/builder/factory/App.java
@@ -0,0 +1,13 @@
+package com.hainv.builder.factory;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ singleton/src/main/java/com/hainv/builder/abstractfactory/singleton/App.java
@@ -0,0 +1,13 @@
+package com.hainv.builder.abstractfactory.singleton;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
